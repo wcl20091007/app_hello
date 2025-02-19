8ba16c76ce1dbb8e9f36026633aa879e21b3faa3
Fong Siu Man <siuman.fong@my.jcu.edu.au>
2025-02-19 15:53:11 +1000

    update init

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
-define('APP_KEY', '');
-define('APP_REFERENCE', '');
-define('APP_CODE', '');
+define('APP_KEY', 'd7575cdf7c171f778c14');
+define('APP_REFERENCE', 'APP2494');
+define('APP_CODE', 'hello_app_test');
